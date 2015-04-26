90744032cdd8a00d776b1c101b8f82ba6e663d0d
Stefan Junker <code@stefanjunker.de>
2015-04-26 13:48:42 -0700

    Add www_go image source, a very simple Go webserver

---
+++ src/www_go/www.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    root_dir := os.Args[1]
+    port := ":" + os.Args[2]
+    fmt.Printf("Serving files from %v on port %v", root_dir, port)
+    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(root_dir))))
+    if err := http.ListenAndServe(port, nil); err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
