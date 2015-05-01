ed8c0571b5578a0453134d21d5cf0c10afac3d28
Stefan Junker <code@stefanjunker.de>
2015-04-30 23:19:49 -0700

    www_go: check for arguments and fix format

--- src/www_go/www.go
+++ src/www_go/www.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    root_dir := os.Args[1]
-    port := ":" + os.Args[2]
-    fmt.Printf("Serving files from %v on port %v", root_dir, port)
-    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(root_dir))))
-    if err := http.ListenAndServe(port, nil); err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	if len(os.Args) < 2 {
+		fmt.Printf("Not enough arguments.")
+		exit(1)
+	}
+	root_dir := os.Args[1]
+	port := ":" + os.Args[2]
+	fmt.Printf("Serving files from %v on port %v", root_dir, port)
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(root_dir))))
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
